3775ee2ff53d8277ab6e46f838da8f80cedba733
m3Rzy <gentledemeiz@gmail.com>
2024-11-19 15:41:46 +0300

    start

---
+++ src/main/java/com/theft/curcon/CurconApplication.java
@@ -0,0 +1,13 @@
+package com.theft.curcon;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CurconApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CurconApplication.class, args);
+	}
+
+}
